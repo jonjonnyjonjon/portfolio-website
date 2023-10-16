e5df7a86157fb673325e36d48e79d755b1461abf
jonjonnyjonjon <jonathanwong99@gmail.com>
2023-10-16 18:37:00 +0800

    fix image slider

--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -5,12 +5,12 @@ import { useEffect } from "react";
 const Intro = () => {
 	useEffect(() => {
 		// https://codepen.io/Coding_Journey/pen/QWdQraQ
-		const slider = document.querySelector("#image-comparison-slider");
+		const slider = document.querySelector("#imageComparisonSlider");
 		const sliderImgWrapper = document.querySelector(
-			"#image-comparison-slider .img-wrapper"
+			"#imageComparisonSlider .imgWrapper"
 		);
 		const sliderHandle = document.querySelector(
-			"#image-comparison-slider .handle"
+			"#imageComparisonSlider .handle"
 		);
 
 		slider.addEventListener("mousemove", sliderMouseMove);
@@ -27,7 +27,10 @@ const Intro = () => {
 			if (mouseX < 0) mouseX = 0;
 			else if (mouseX > sliderWidth) mouseX = sliderWidth;
 
-			sliderImgWrapper.style.width = `${((1 - mouseX / sliderWidth) *100).toFixed(4)}%`;
+			sliderImgWrapper.style.width = `${(
+				(1 - mouseX / sliderWidth) *
+				100
+			).toFixed(4)}%`;
 			sliderHandle.style.left = `calc(${((mouseX / sliderWidth) * 100).toFixed(
 				4
 			)}% - ${sliderHandleWidth / 2}px)`;
@@ -56,33 +59,35 @@ const Intro = () => {
 	}, []);
 
 	return (
-		<div className="mx-auto max-w-2xl py-32" id="intro">
-			<div className="text-center">
-				<div className="flex flex-col md:flex-row items-center justify-center gap-6">
-					<div className="flex flex-col items-start">
-						<p className="text-2xl">Hello 👋</p>
-						<p className="text-2xl">I am Jonathan</p>
-						<p className="text-2xl">A Software Engineer</p>
-					</div>
-
-					<div id="image-comparison-slider">
-						<img src={PixelAvatar} alt="before" />
-
-						<div className="img-wrapper">
-							<img src={OriginalAvatar} alt="after" />
+		<>
+			<div className="mx-auto max-w-2xl py-32" id="intro">
+				<div className="text-center">
+					<div className="flex flex-col md:flex-row items-center justify-center gap-6">
+						<div className="flex flex-col items-start">
+							<p className="text-2xl">Hello 👋</p>
+							<p className="text-2xl">I am Jonathan</p>
+							<p className="text-2xl">A Software Engineer</p>
 						</div>
-						<div className="handle">
-							<div className="handle-line"></div>
-							<div className="handle-circle">
-								<i className="fas fa-chevron-left"></i>
-								<i className="fas fa-chevron-right"></i>
+
+						<div id="imageComparisonSlider">
+							<img src={OriginalAvatar} alt="before" />
+
+							<div className="imgWrapper">
+								<img src={PixelAvatar} alt="after" />
 							</div>
-							<div className="handle-line"></div>
+							<div className="handle">
+								<div className="handleLine"></div>
+								<div className="handleCircle">
+									<i className="fas fa-chevron-left"></i>
+									<i className="fas fa-chevron-right"></i>
+								</div>
+								<div className="handleLine"></div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
